603d6c9bcf242d7236a4f47c6c3b062672926bfb
Aluon <paulacanoroman@gmail.com>
2023-10-04 23:18:06 +0200

    consumo API console.log

--- pt-041023/components/MealIngredients.jsx
+++ pt-041023/components/MealIngredients.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const MealIngredients = () => {
+
+
+    const [dataAPI, setDataAPI] = useState([])
+
+    useEffect(() => {
+      const apiUrl = `https://www.themealdb.com/api/json/v1/1/random.php`;
+      axios.get(apiUrl)
+        .then( response => console.log(response.data.meals))
+        .catch(error => console.log("error deteceted",error))
+    }, [])
+    
+
+
   return (
     <div>MealIngredients</div>
   )
