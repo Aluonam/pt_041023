c90f46f92ce000878dbdc80c2d85accc4f438f29
Aluon <paulacanoroman@gmail.com>
2023-10-04 23:46:52 +0200

    convertir objeto en array de clave y valor

--- pt-041023/components/MealIngredients.jsx
+++ pt-041023/components/MealIngredients.jsx
@@ -9,14 +9,25 @@ const MealIngredients = () => {
     useEffect(() => {
       const apiUrl = `https://www.themealdb.com/api/json/v1/1/random.php`;
       axios.get(apiUrl)
-        .then( response => console.log(response.data.meals))
+        .then( response => setDataAPI(response.data.meals[0]))
         .catch(error => console.log("error deteceted",error))
     }, [])
     
 
+    // La llamada API devuelve un objeto
+    // Convierto el objeto con calve valor a un array que tambien tendrá clave valor. con .entries se crea un subarray por así decirlo.
+    const arrayObj = Object.entries(dataAPI)
+
+    // se filtra para sacar dentro de la clave [0] con .startsWith() que permite comparar el string y además que el valor [1] sea distinto de vacio.
+    const ingredientes = arrayObj.filter((elementoActual)=>elementoActual[0].startsWith("strIngredient") && elementoActual[1]!=="")
+    console.log("ingredientes",ingredientes)
+
+    // Recorrer con .map para sacar listado
 
   return (
-    <div>MealIngredients</div>
+    <div>MealIngredients
+        {/* {ingredientes} */}
+    </div>
   )
 }
 
